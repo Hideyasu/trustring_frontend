52f048242807b307d48f270ca9e80f0bcac24446
Hideyasu Yamaguchi <kutuzov1228@gmail.com>
2020-12-24 00:10:40 +0900

    second commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
         router: {
-          base: '/trustring/',
+          base: '/trustring_frontend/',
         },
       }
     : {
